0c21049e9f0f9b8eb8dc4a5e5973af7369081a49
PRAGYA-J <hit18ee.pragyajha@gmail.com>
2023-01-30 22:43:26 +0530

    navigation and create button

---
+++ frontend/src/Pages/Groups.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import * as Mui from '@mui/material';
+import HomeNavbar from '../Components/Navbar/homeNavbar';
+import Divider from '@mui/material/Divider';
+
+export default function Groups(){
+    return(
+        <>
+        <HomeNavbar/>
+        <Mui.Card  className="mx-4 my-1">
+                <Mui.CardContent className="d-flex justify-content-between">
+                    <div className="w-40">
+                        <h3 className="w-100 mx-2">Explore Groups</h3>
+                    </div>
+                    <div className="w-45">
+                    <Mui.Stack className="d-flex mx-2" spacing={2} direction="row">
+                            <Mui.Button variant="contained" color="primary" align="right">Create your own Group</Mui.Button>
+                            <Mui.Button variant="contained" color="secondary" align="right">Join a random group</Mui.Button>
+                        </Mui.Stack>
+                    </div>
+                </Mui.CardContent>
+                </Mui.Card>
+        </>
+    );
+}
